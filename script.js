bcba68dc31900b6a9e8673b369a13e693afca2bd
FabienneHirwa <65540469+FabienneHirwa@users.noreply.github.com>
2020-10-05 09:38:31 +0200

    init

--- script.js
+++ script.js
@@ -58,11 +58,10 @@ $(function () {
       $("form#form").on('submit',function(event){
           event.preventDefault();
           let name = $("input#name").val();
-          let email = $("input#email").val();
-          let message = $("textarea#message").val();
+       
 
           if ($("input#name").val() && $("input#email").val()){
-              alert ("Hey " + name + ", Thanks for reaching out, we'll get back to you shortly...cheers");
+              alert ("Hey " + name + ", Thanks for reaching out us!");
           }
           else {
               alert("Please provide your correct name and email!");
